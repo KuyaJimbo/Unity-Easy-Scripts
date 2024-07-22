39c1815357e239cad958e99098dbba5698487f69
James Ocampo <124105215+KuyaJimbo@users.noreply.github.com>
2024-07-22 18:50:15 -0400

    Update PlayerMoveTurn.cs

--- PlayerMoveTurn.cs
+++ PlayerMoveTurn.cs
@@ -7,25 +7,36 @@ public class PlayerMoveTurn : MonoBehaviour
     // Speed for movement and rotation
     public float moveSpeed = 5f;
     public float rotationSpeed = 100f;
+    // add rigidbody
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
         // Handle movement manually
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime, Space.Self);
+            // Move the player forward with addForce
+            rb.AddForce(transform.forward * moveSpeed);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime, Space.Self);
+            // Move the player backward with addForce
+            rb.AddForce(-transform.forward * moveSpeed);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime, Space.Self);
+            // Move the player left with addForce
+            rb.AddForce(-transform.right * moveSpeed);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.Self);
+            // Move the player right with addForce
+            rb.AddForce(transform.right * moveSpeed);
         }
         // Handle rotation
         if (Input.GetKey(KeyCode.LeftArrow))
@@ -38,3 +49,4 @@ public class PlayerMoveTurn : MonoBehaviour
         }
     }
 }
+
